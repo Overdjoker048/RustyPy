01a51ca57c24ee8f62460b0aa90f25900141af0d
Overdjoker048 <92109090+Overdjoker048@users.noreply.github.com>
2026-01-26 22:56:14 +0100

    Delete header.py

--- header.py
+++
@@ -1,9 +0,0 @@
-from gc import disable
-disable()
-del disable
-from sys import stdout, dont_write_bytecode
-stdout.reconfigure(encoding="utf-8")
-dont_write_bytecode = True
-del dont_write_bytecode
-del stdout
-del print
